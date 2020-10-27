667876c19ad214a82e140da785f42d2e1900035c
EduardoIvorra <62476207+EduardoIvorra@users.noreply.github.com>
2020-10-27 23:35:55 +0100

    5.1

---
+++ MyApplication/app/src/test/java/com/example/myapplication/MainActivityTest.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+
+import junit.framework.TestCase;
+
+public class MainActivityTest extends TestCase {
+
+    public void testPlay() {
+        //Intent play = launchActivity<Juego>();
+
+    }
+
+    public void testProfile() {
+    }
+
+    public void testSettings() {
+    }
+
+    public void testRanking() {
+    }
+
+    public void testExit() {
+    }
+}
