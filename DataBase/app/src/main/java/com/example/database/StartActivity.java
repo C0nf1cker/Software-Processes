6043bd9dbc35379b504fbbaa9f034fa8f13b4d9b
eduvilla97 <eduvilla99@gmail.com>
2020-11-09 11:21:29 +0100

    Pantalla de inicio de la App

---
+++ DataBase/app/src/main/java/com/example/database/StartActivity.java
@@ -0,0 +1,21 @@
+package com.example.database;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+    public void startApplication(View view){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+}
