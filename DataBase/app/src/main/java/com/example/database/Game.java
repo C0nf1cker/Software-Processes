5b0b21431e2727f4beb2f199703b8d7645874154
eduvilla97 <eduvilla99@gmail.com>
2020-12-06 14:22:36 +0100

    no message

--- DataBase/app/src/main/java/com/example/database/Game.java
+++ DataBase/app/src/main/java/com/example/database/Game.java
@@ -27,12 +27,16 @@ public class Game extends AppCompatActivity {
     private UsersDataBase ddbb;
     //showValue es el textview correspondiente a la puntuación que me va a ir variando en función de los clicks dados al círculo
     TextView showValue;
+    //textview donde plasmaremos por pantalla el tiempo restante
+    TextView timeRemining;
     // el counter es el contador de los clicks dados al círculo necesarios para contar la puntuación
     public int counter;
     //necesitamos unbooleano pressed para saber si se ha presionado en una partida el círculo ya que,
     //en caso de que si, no cuente como puntuación múltiples clicks sobre el mismo círculo del mismo nivel, sería ilógico
-    public boolean pressed = false;
-
+    public boolean objetivoPressed = false;
+    //el temporizador para el juego
+    private Temporizador timer;
+    private boolean restoPressed = false;
 
 
     @Override
@@ -41,14 +45,26 @@ public class Game extends AppCompatActivity {
         setContentView(R.layout.activity_juego);
         //Linkamos el texto para la puntuación de la partida con su correspondiente en el layout
         showValue = (TextView) findViewById(R.id.counterValue);
-        //inicializamos la BBDD
+        timeRemining = (TextView) findViewById(R.id.timeText);
+        //inicializamos la BBDD y el temporizador
         ddbb = new UsersDataBase(this);
+        timer = new Temporizador(timeRemining);
         //Tomamos el email del usuario logeado que nos manda el menú
         currentUserEmail = getIntent().getStringExtra("userEmail");
-        //innicializamos pressed a false y counter a 0
-        pressed = false;
         counter = 0;
-        //coloca el círculo con id: button12 en posiciones aleatorias
+        //Esperar a que el jugador este listo para empezar la partida
+
+        //empezar la partida
+        timer.star();
+        colocarBoton();
+    }
+
+    private void notificarRessultado() {
+        Toast.makeText(this, "Lo has encontrado "+counter+" veces!", Toast.LENGTH_SHORT).show();
+    }
+
+
+    private void colocarBoton(){
         Button button = (Button) findViewById(R.id.button12);
         AbsoluteLayout.LayoutParams absParams =
                 (AbsoluteLayout.LayoutParams) button.getLayoutParams();
@@ -76,7 +92,8 @@ public class Game extends AppCompatActivity {
         //Cuando salimos del juego y es un usuario loggeado tenemos que actualiar su puntuación
         if (currentUserEmail != null) {
             currentUser = ddbb.getUser(currentUserEmail);
-            ddbb.updateScore(currentUser.getEmail(), counter+currentUser.getScore());
+            if(currentUser.getScore()<counter)//actualizamos la puntuacion si supera el record anterior del jugador
+                ddbb.updateScore(currentUser.getEmail(), counter);
         }
         //Devolvemos al menú el email del usuario logeado para que mantenga la sesión iniciada
         main.putExtra("userEmail", this.currentUserEmail);
@@ -91,14 +108,17 @@ public class Game extends AppCompatActivity {
      *
      * @param view
      */
-    public void victoryNotification_increaseScore(View view) {
-        if (!pressed) {
-            Toast.makeText(this, "lo has encontrado!", Toast.LENGTH_SHORT).show();
-            //en cada click de este botón el counter se incrementará
-            counter++;
-            showValue.setText(Integer.toString(counter));
-        }
-        pressed = true;
+    public void clickOnObjetive(View view) {
+        //en cada click de este botón el counter se incrementará
+        counter++;
+        showValue.setText(Integer.toString(counter));
+        //se le añade tiempo al jugador y vuelve a contar
+        timer.acertar();
+        playAgain();
+    }
+
+    public void clickOutObjetive(View view){
+                
     }
 
     /**
@@ -108,24 +128,7 @@ public class Game extends AppCompatActivity {
      * @param view
      */
     public void playAgain(View view) {
-        //innicializamos pressed a false
-        pressed = false;
-        //coloca el círculo con id: button12 en posiciones aleatorias
-        Button button = (Button) findViewById(R.id.button12);
-        AbsoluteLayout.LayoutParams absParams =
-                (AbsoluteLayout.LayoutParams) button.getLayoutParams();
-
-        DisplayMetrics displaymetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
-        int width = displaymetrics.widthPixels - 200;
-        int height = displaymetrics.heightPixels - 500;
-
-
-        Random r = new Random();
-
-        absParams.x = r.nextInt(width);
-        absParams.y = r.nextInt(height);
-        button.setLayoutParams(absParams);
+        colocarBoton();
         //necesario para mostrar la puntuación síncrona a tiempo real en la casilla textView counterValue
         showValue = (TextView) findViewById(R.id.counterValue);
     }
